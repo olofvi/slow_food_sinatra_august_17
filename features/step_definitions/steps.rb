dce93a666c01b899cd0e5b83f5369d266d8ebd8c
Amanda <29342045+Sajaas@users.noreply.github.com>
2017-09-07 13:59:22 +0200

    fixed test for dish price

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -10,10 +10,9 @@ Given(/^there is a dish named "([^"]*)" in our database$/) do |dish_name|
   Dish.create(name: dish_name)
 end
 
-Given(/^there is a dish price of "([^"]*)" in our database$/) do |dish_price|
-  Dish.create(price: dish_price)
+Given(/^its dish price is "([^"]*)" in our database$/) do |dish_price|
+  Dish.create(name: dish_price)
 end
-
 
 Then(/^I should se a message saying "([^"]*)"$/) do |message|
   expect(page).to have_content message
