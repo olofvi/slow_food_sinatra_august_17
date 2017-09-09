900b7ebdd73ce1e4048d868cee768c789fe5c6d3
magnus-thor <magnus.thor.ar@gmail.com>
2017-09-09 16:57:33 +0200

    added Order_item, rspec failing

--- spec/order_item_spec.rb
+++ spec/order_item_spec.rb
@@ -1,8 +1,8 @@
 require './lib/models/order_item.rb'
 
-describe Dish do
+describe Order_item do
   it{is_expected.to have_property :id}
   it{is_expected.to have_many :order_items}
   it{is_expected.to belongs_to :user}
   it{is_expected.to validates_presence_of :user}
-end 
+end
