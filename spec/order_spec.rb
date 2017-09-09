767ae31d864eb7980632095e6b16aff81cc05d2b
olofvi <olofvi89@gmail.com>
2017-09-09 16:38:45 +0200

    WIP made some rspec

---
+++ spec/order_spec.rb
@@ -0,0 +1,25 @@
+require './lib/models/order.rb'
+
+describe Dish do
+  it{is_expected.to have_property :id}
+  it{is_expected.to have_many :order_items}
+  it{is_expected.to belongs_to :user}
+  it{is_expected.to validates_presence_of :user}
+
+   let(:buyer) { User.create!(username: 'Buyer',
+                             password: 'password',
+                             phone_number: '123456',
+                             email: 'buyer@test.com') }
+
+   let(:item_1) { Dish.create(name: 'Test 1',
+                 price: 50) }
+   let(:item_2) { Dish.create(name: 'Test 2', price: 10) }
+
+   subject { described_class.create(user: buyer) }
+
+
+   it 'adds dish to basket as basket_item' do
+     subject.add_item(item_1, item_1.price, 2)
+     expect(subject.order_items.count).to eql 1
+   end
+ end
