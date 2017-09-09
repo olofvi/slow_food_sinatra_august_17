a42519b32df5315117a2f97a335fc4212f56b9ea
magnus-thor <magnus.thor.ar@gmail.com>
2017-09-09 16:57:33 +0200

    added Order_item, rspec failing

---
+++ lib/models/order_item.rb
@@ -0,0 +1,16 @@
+class OrderItem
+  include DataMapper::Resource
+
+  property :id, Serial, key: true
+  property :price, Integer
+  property :quantity, Integer
+
+  has 1, :dish, through: Resource
+  belongs_to :order
+
+  validates_presence_of :dish
+
+  def item
+    Dish.get(self.dish.is)
+  end
+end
